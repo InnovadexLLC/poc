0363fd93d6c3f92908bb0de3a981df09ce73eaf7
sumeet rohatgi <sum.rohatgi@gmail.com>
2013-12-12 12:41:23 -0800

    changed name to local-repo

--- addjar.sh
+++ addjar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ##mvn deploy:deploy-file  -Dfile=/Users/sumeetrohatgi/Downloads/sqljdbc_4.0/enu/sqljdbc4.jar -DartifactId=sqljdbc4 -Dversion=4.0 -DgroupId=sqljdbc4 -Dpackaging=jar -Durl=file:repo
-mvn deploy:deploy-file  -Dfile=$1 -DartifactId=$(basename -s .jar $1) -Dversion=$2 -DgroupId=$(basename -s .jar $1) -Dpackaging=jar -Durl=file:repo
+mvn deploy:deploy-file  -Dfile=$1 -DartifactId=$(basename -s .jar $1) -Dversion=$2 -DgroupId=$(basename -s .jar $1) -Dpackaging=jar -Durl=file:local-repo
